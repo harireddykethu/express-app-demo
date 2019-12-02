04d9a776e4f56ca02e037b7c620e58d93d577475
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-02 16:03:46 +0530

    done

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hi there!');
+const express = require('express');
+
+const app = express();
+
+//  Use port 3000 if the environment doesn't specify one
+
+const PORT = process.env.PORT || 3000;
+
+//  home route
+app.get('/', (request, response) => {
+  response.send({
+    on: new Date(),
+    message: 'Hi there!'
+  });
+});
+
+//  Listen to port
+app.listen(PORT);
