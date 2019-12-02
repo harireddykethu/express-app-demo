8a9c7c1667281d4d5113d65c64651d74767f93d3
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-02 20:36:59 +0530

    done

--- home-routes.js
+++ home-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const fs = require('fs');
 const util = require('util');
+const https = require('https');
+const axios = require('axios').default;
 
 router.get('/', async (request, response) => {
   const promise = util.promisify(fs.readFile);
@@ -14,6 +16,26 @@ router.get('/home', (request, response) => {
   response.sendFile('./index.html', {
     root: '.'
   });
+});
+
+router.get('/todo', async (request, response) => {
+  let result;
+  try {
+    //  More information about Axios here: https://www.npmjs.com/package/axios
+    //  And here: https://github.com/axios/axios
+    result = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    response.send({
+      on: new Date(),
+      message: 'Request successful',
+      result: result.data
+    });
+  } catch (error) {
+    console.log('ERROR', error);
+    response.status(500).send({
+      on: new Date(),
+      message: 'Server error'
+    });
+  }
 });
 
 module.exports = router;
