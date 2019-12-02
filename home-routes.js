0a23cb9bfe02800284574e407fc3360f55e7a20d
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-02 18:08:22 +0530

    done

--- home-routes.js
+++ home-routes.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
+const fs = require('fs');
+const util = require('util');
 
-router.get('/', (request, response) => {
-  response.send({
-    on: new Date(),
-    message: 'Hi there! 1'
+router.get('/', async (request, response) => {
+  const promise = util.promisify(fs.readFile);
+  const fileContents = await promise('index.html', { encoding: 'UTF8' });
+  response.send(fileContents);
+});
+
+router.get('/home', (request, response) => {
+  // More information here: http://expressjs.com/en/5x/api.html#res.sendFile
+  //  root is required
+  response.sendFile('./index.html', {
+    root: '.'
   });
 });
 
