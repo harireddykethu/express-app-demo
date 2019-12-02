e7074d10f9e06e33bd4b0d5419c564832d947025
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-02 17:47:09 +0530

    done

--- app.js
+++ app.js
@@ -40,14 +40,36 @@ app.get('/api/items', (request, response) => {
 
 //  Regex in route parameters
 //  More information here: https://expressjs.com/en/guide/routing.html
+//  More information on Http Status Codes here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 app.get('/api/items/:id([0-9]+)', (request, response) => {
   const id = +request.params.id;
 
-  response.send({
-    on: new Date(),
-    message: 'GET successful',
-    id
-  });
+  //  More information on status() is here: http://expressjs.com/en/5x/api.html#res.status
+
+  if (id > 100) {
+    response.status(400).send({
+      on: new Date(),
+      message: 'GET error',
+      id
+    });
+  } else {
+    if (id < 10) {
+      //  Explicit status set using node API
+      response.statusCode = 201; //  ok > creation status code
+      response.send({
+        on: new Date(),
+        message: 'GET successful',
+        id
+      });
+    } else {
+      //  Unless response.statusCode = xxx is set explicitly, status 200 is default for response.send()
+      response.send({
+        on: new Date(),
+        message: 'GET successful',
+        id
+      });
+    }
+  }
 });
 
 app.get('/api/items/:title(\\w+)', (request, response) => {
